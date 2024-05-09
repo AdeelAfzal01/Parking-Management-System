dfcfa8aa751a69df3f2c3e8c5d6ecca1923dc152
Adeel <adeelafzal01@gmail.com>
2024-05-09 07:26:58 +0500

    First Commit

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import cv2
+import numpy as np
 from util import get_parking_spots_bboxes, empty_or_not
 
-mask = "data/mask_crop.png"
-video_path = "data/parking_crop_loop.mp4"
+def calc_diff(im1, im2):
+    return np.abs(np.mean(im1) - np.mean(im2))
+
+mask = "data/mask_1920_1080.png"
+video_path = "data/parking_1920_1080_loop.mp4"
 mask = cv2.imread(mask, 0)
 cap = cv2.VideoCapture(video_path)
 connected_components = cv2.connectedComponentsWithStats(mask, 4, cv2.CV_32S)
 spots = get_parking_spots_bboxes(connected_components)
 
 spots_status = [None for j in spots]
+diffs = [None for j in spots]
+
+previous_frame = None
 frame_nmr = 0
 ret = True
 step = 30
@@ -16,12 +23,28 @@ step = 30
 while ret:
     ret, frame = cap.read()
 
-    if frame_nmr % step == 0:
+    if frame_nmr % step == 0 and previous_frame is not None:
         for spot_index, spot in enumerate(spots):
+
+            x1 , y1, w, h = spot
+            spot_crop = frame[y1:y1+h, x1:x1+w, :]
+            diffs[spot_index] = calc_diff(spot_crop, previous_frame[y1:y1+h, x1:x1+w, :])
+
+
+    if frame_nmr % step == 0:
+        if previous_frame is None:
+            arr_ = range(len(spots))
+        else:
+            arr_ = [j for j in np.argsort(diffs) if diffs[j] / np.amax(diffs) > 0.4]
+        for spot_index in arr_:
+            spot = spots[spot_index]
             x1, y1, w, h = spot
             spot_crop = frame[y1:y1+h, x1:x1+w, :]
             spot_status = empty_or_not(spot_crop)
             spots_status[spot_index] = spot_status
+
+    if frame_nmr % step == 0:
+        previous_frame = frame.copy()
 
     for spot_index, spot in enumerate(spots):
         spot_status = spots_status[spot_index]
@@ -31,6 +54,9 @@ while ret:
         else:
             frame = cv2.rectangle(frame, (x1, y1), (x1+w, y1+h), (0, 0, 255), 2)
 
+    cv2.rectangle(frame, (80, 20), (550, 80), (0, 0, 0), -1)
+    cv2.putText(frame, 'Available splots: {} / {}'.format(str(sum(spots_status)), str(len(spots_status))), (100, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+    cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
     cv2.imshow('frame', frame)
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
